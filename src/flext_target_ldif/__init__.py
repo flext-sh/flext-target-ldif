1d6e2d79e35e063ab1df0f375f6a9ec7761a5a36
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 02:46:41 -0300

    chore(codegen): inline lazy __init__.py, eliminate _exports.py sidecar
    Regenerated by 'flext_infra codegen init --apply': PEP 562 lazy imports are
    now inlined directly into each package __init__.py; the separate _exports.py
    lazy-map sidecar is removed (superseded by the inline map).

--- src/flext_target_ldif/__init__.py
+++ src/flext_target_ldif/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from flext_core.lazy import install_lazy_exports
+from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 from flext_target_ldif.__version__ import (
     __author__,
     __author_email__,
@@ -16,89 +16,59 @@ from flext_target_ldif.__version__ import (
     __version__,
     __version_info__,
 )
-from flext_target_ldif._exports import FLEXT_TARGET_LDIF_LAZY_IMPORTS
 
 if TYPE_CHECKING:
-    from flext_ldif import d as d, e as e, h as h, r as r, s as s, x as x
-    from flext_target_ldif.api import (
-        FlextTargetLdifService as FlextTargetLdifService,
-        target_ldif as target_ldif,
-    )
-    from flext_target_ldif.cli import (
-        FlextTargetLdifCli as FlextTargetLdifCli,
-        main as main,
-    )
-    from flext_target_ldif.constants import (
-        FlextTargetLdifConstants as FlextTargetLdifConstants,
-        c as c,
-    )
-    from flext_target_ldif.models import (
-        FlextTargetLdifModels as FlextTargetLdifModels,
-        m as m,
-    )
-    from flext_target_ldif.protocols import (
-        FlextTargetLdifProtocols as FlextTargetLdifProtocols,
-        p as p,
-    )
-    from flext_target_ldif.settings import (
-        FlextTargetLdifSettings as FlextTargetLdifSettings,
-    )
-    from flext_target_ldif.typings import (
-        FlextTargetLdifTypes as FlextTargetLdifTypes,
-        t as t,
-    )
-    from flext_target_ldif.utilities import (
-        FlextTargetLdifUtilities as FlextTargetLdifUtilities,
-        u as u,
-    )
-
-
-_LAZY_IMPORTS = FLEXT_TARGET_LDIF_LAZY_IMPORTS
-
-
-_EAGER_EXPORTS = (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-    __version_info__,
+    from flext_ldif import d, e, h, r, s, x
+    from flext_target_ldif.api import FlextTargetLdifService
+    from flext_target_ldif.cli import FlextTargetLdifCli, main
+    from flext_target_ldif.constants import FlextTargetLdifConstants, c
+    from flext_target_ldif.models import FlextTargetLdifModels, m
+    from flext_target_ldif.protocols import FlextTargetLdifProtocols, p
+    from flext_target_ldif.settings import FlextTargetLdifSettings
+    from flext_target_ldif.typings import FlextTargetLdifTypes, t
+    from flext_target_ldif.utilities import FlextTargetLdifUtilities, u
+_LAZY_IMPORTS = build_lazy_import_map(
+    {
+        ".api": (
+            "FlextTargetLdifService",
+            "target_ldif",
+        ),
+        ".cli": (
+            "FlextTargetLdifCli",
+            "main",
+        ),
+        ".constants": (
+            "FlextTargetLdifConstants",
+            "c",
+        ),
+        ".models": (
+            "FlextTargetLdifModels",
+            "m",
+        ),
+        ".protocols": (
+            "FlextTargetLdifProtocols",
+            "p",
+        ),
+        ".settings": ("FlextTargetLdifSettings",),
+        ".typings": (
+            "FlextTargetLdifTypes",
+            "t",
+        ),
+        ".utilities": (
+            "FlextTargetLdifUtilities",
+            "u",
+        ),
+        "flext_ldif": (
+            "d",
+            "e",
+            "h",
+            "r",
+            "s",
+            "x",
+        ),
+    },
 )
 
-
-_PUBLIC_EXPORTS: tuple[str, ...] = (
-    "FlextTargetLdifCli",
-    "FlextTargetLdifConstants",
-    "FlextTargetLdifModels",
-    "FlextTargetLdifProtocols",
-    "FlextTargetLdifService",
-    "FlextTargetLdifSettings",
-    "FlextTargetLdifTypes",
-    "FlextTargetLdifUtilities",
-    "target_ldif",
-    "__author__",
-    "__author_email__",
-    "__description__",
-    "__license__",
-    "__title__",
-    "__url__",
-    "__version__",
-    "__version_info__",
-    "c",
-    "d",
-    "e",
-    "h",
-    "m",
-    "main",
-    "p",
-    "r",
-    "s",
-    "t",
-    "u",
-    "x",
-)
 
 __all__: tuple[str, ...] = (
     "FlextTargetLdifCli",
@@ -137,5 +107,5 @@ install_lazy_exports(
     __name__,
     globals(),
     _LAZY_IMPORTS,
-    public_exports=_PUBLIC_EXPORTS,
+    public_exports=__all__,
 )
