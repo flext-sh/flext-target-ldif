e96a4432433400478f9449a9d51d05290386d244
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 14:40:37 -0300

    chore(gen): converge projections at the integration tip (fleet fixed point)

--- src/flext_target_ldif/__init__.py
+++ src/flext_target_ldif/__init__.py
@@ -20,9 +20,8 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_ldif import s
-
     from flext_core import d, e, h, r, x
+    from flext_ldif import s
 
     from .__version__ import FlextTargetLdifVersion
     from ._config import FlextTargetLdifConfig, config
